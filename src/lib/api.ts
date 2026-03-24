2ae9fdecb0aceb16f0f46a4ca5819a12214361ca
vanumu_shashi <vanumshashi@gmail.com>
2026-03-24 17:06:37 +0530

    updated code

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://127.0.0.1:8000/api/",
+  // baseURL: "http://127.0.0.1:8000/api/",
+  baseURL: "https://translator-be-drk5.onrender.com/api/",
 });
 
 export default API;
